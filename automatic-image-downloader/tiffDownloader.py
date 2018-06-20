c153f096d9d82a287598febe87cac924423b937a
adescay <andrew.escay@gmail.com>
2018-06-20 15:45:38 -0700

    Clean script for tiffDownloader.py to be pep8 compliant

--- automatic-image-downloader/tiffDownloader.py
+++ automatic-image-downloader/tiffDownloader.py
@@ -2,35 +2,42 @@ import sys
 import urllib
 import os.path
 
-# command line tool to easily download a batch of geotiff files (.tif) from DigitalGlobe Open Data Program: http://digitalglobe.com/opendata/
+"""
+Command line tool to easily download a batch of GeoTIFF files (.tif)
+from DigitalGlobe Open Data Program: http://digitalglobe.com/opendata/
+"""
 
-def downloadFiles(fileUrlList, overwriteIfExists=False):
-	for fileUrl in fileUrlList:
-		filename = fileUrl[fileUrl.rfind("/")+1:]
-		if overwriteIfExists or not os.path.isfile(filename):
-			opener = urllib.URLopener()
-			opener.retrieve(fileUrl, filename)
-			print('downloaded file: {}'.format(filename))
 
-def filterListByExtension(fileUrlList, extension):
-	result = []
-	for fileUrl in fileUrlList:
-		if fileUrl.endswith(extension):
-			result.append(fileUrl)
-			print('added to queue: {}'.format(fileUrl))
-	return result
+def download_files(urls, overwrite_if_exists=False):
+    for fileUrl in urls:
+        filename = fileUrl[fileUrl.rfind("/")+1:]
+        if overwrite_if_exists or not os.path.isfile(filename):
+            opener = urllib.URLopener()
+            opener.retrieve(fileUrl, filename)
+            print('downloaded file: {}'.format(filename))
+
+
+def filter_list_by_extension(urls, extension):
+    result = []
+    for fileUrl in urls:
+        if fileUrl.endswith(extension):
+            result.append(fileUrl)
+            print('added to queue: {}'.format(fileUrl))
+    return result
+
 
 # execution starts here. command line args processing.
 if len(sys.argv) == 2 and sys.argv[1] == '-h':
-	print('\n\n   Usage: python tiffDownloader.py <urls_file> \n\n   Each URL must be on a new line.')
+    print('\n\n   Usage: python tiffDownloader.py <urls_file> \n\n   Each URL must be on a new line.')
 elif len(sys.argv) > 1:
-	fileWithUrlList = sys.argv[1]
-	with open(fileWithUrlList) as f:
-		content = f.readlines()
-		# remove whitespace characters at the end of each line
-		content = [x.strip() for x in content] 
-		tiffList = filterListByExtension(content, '.tif')
-		downloadFiles(tiffList)
+    file_list = sys.argv[1]
+    with open(file_list) as f:
+        content = f.readlines()
+        # remove whitespace characters at the end of each line
+        content = [x.strip() for x in content]
+        tiffList = filter_list_by_extension(content, '.tif')
+        download_files(tiffList)
 else:
-	print('error: required command line argument missing. \n\n Syntax: python httpDownloader.py <urls_file> \n\n Each URL must be on a new line.')
-	sys.exit(0)
+    print('error: required command line argument missing. \n\n Syntax: python httpDownloader.py <urls_file> \n\n Each '
+          'URL must be on a new line.')
+    sys.exit(0)
