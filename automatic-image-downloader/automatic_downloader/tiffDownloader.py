392633ba87f3f8bbada2878f731793abca48ef5d
adescay <andrew.escay@gmail.com>
2018-06-21 12:31:14 -0700

    Fix file system and add other required arguments:

--- automatic-image-downloader/automatic_downloader/tiffDownloader.py
+++ automatic-image-downloader/automatic_downloader/tiffDownloader.py
@@ -1,6 +1,8 @@
 import sys
 import urllib
+import os
 import os.path
+
 
 """
 Command line tool to easily download a batch of GeoTIFF files (.tif)
@@ -28,16 +30,21 @@ def filter_list_by_extension(urls, extension):
 
 # execution starts here. command line args processing.
 if len(sys.argv) == 2 and sys.argv[1] == '-h':
-    print('\n\n   Usage: python tiffDownloader.py <urls_file> \n\n   Each URL must be on a new line.')
-elif len(sys.argv) > 1:
+    print('\n\n   Usage: python tiffDownloader.py <urls_file> <hurricane_name> \n\n   Each URL must be on a new line.')
+elif sys.argv[1] and sys.argv[2]:
+    os.chdir('../data')
     file_list = sys.argv[1]
     with open(file_list) as f:
-        content = f.readlines()
-        # remove whitespace characters at the end of each line
-        content = [x.strip() for x in content]
-        tiffList = filter_list_by_extension(content, '.tif')
-        download_files(tiffList)
+        event = sys.argv[2]
+        print ('../data/%s' % event)
+    #     os.mkdir('../data/@event')
+    #     os.chdir('../data/@event')
+    #     content = f.readlines()
+    #     # remove whitespace characters at the end of each line
+    #     content = [x.strip() for x in content]
+    #     tiffList = filter_list_by_extension(content, '.tif')
+    #     download_files(tiffList)
 else:
-    print('error: required command line argument missing. \n\n Syntax: python httpDownloader.py <urls_file> \n\n Each '
-          'URL must be on a new line.')
+    print('error: required command line argument missing. \n\n Syntax: python httpDownloader.py <urls_file> '
+          '<hurricane_name> \n\n Each URL must be on a new line.')
     sys.exit(0)
