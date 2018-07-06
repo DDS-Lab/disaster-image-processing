9d97cf52afb9be10cf2cfcbdf1aec3eef0cc5c96
argideritzalpea <crh2ke@virginia.edu>
2018-07-06 16:31:53 -0700

    modifying latlong converter

--- utils/sentinel_sandbox.py
+++ utils/sentinel_sandbox.py
@@ -1,4 +1,4 @@
-import sentinelhub as stnl
+#import sentinelhub as stnl
 import numpy as np
 import geopandas as gpd
 from shapely.geometry import shape, Polygon, MultiPolygon, MultiLineString
@@ -7,10 +7,24 @@ from matplotlib.patches import Polygon as plt_polygon
 from fiona.crs import from_epsg
 import csv
 import gdal
+from os import listdir
+from os.path import isfile, join
 
 #from mpl_toolkits.basemap import Basemap  # Avaliable here: https://github.com/matplotlib/basemap
 
 inputfile = '/Users/Chris/Desktop/hey/dssg/test2.json'
+tiffolder = '/Users/Chris/Desktop/hey/dssg/sample_tiff/'
+image_lat_longs_w_id = '/Users/Chris/Desktop/hey/dssg/tifRange-tiles-run-1.csv'
+onlyfiles = [f for f in listdir(tiffolder) if isfile(join(tiffolder, f))]
+
+with open(image_lat_longs_w_id, 'r') as f:
+	reader = csv.reader(f)
+	next(reader)
+	image_ids = [r for r in reader]
+
+testd = image_ids[0]
+
+#smalltiffidfile = '/Users/Chris/Desktop/hey/dssg/tifRange-tiles-run-1.csv'
 # image_lat_longs_w_id = csv.DictReader(open("image_id_ll.csv"))
 driver = gdal.GetDriverByName('GTiff')
 
@@ -18,16 +32,14 @@ driver = gdal.GetDriverByName('GTiff')
 geo_json = gpd.read_file(inputfile)
 study_area = geo_json.copy()
 
-
 def getTransform(img_id):
-	dataset = gdal.Open(img_id)
+	path = tiffolder + '/' + img_id
+	print(path)
+	dataset = gdal.Open(path)
 	band = dataset.GetRasterBand(1)
-
 	cols = dataset.RasterXSize
 	rows = dataset.RasterYSize
-
 	transform = dataset.GetGeoTransform()
-
 	xOrigin = transform[0]
 	yOrigin = transform[3]
 	pixelWidth = transform[1]
@@ -35,19 +47,20 @@ def getTransform(img_id):
 	return xOrigin, yOrigin, pixelWidth, pixelHeight, cols, rows
 
 def convert(xOrigin, yOrigin, pixelWidth, pixelHeight, cols, rows):
-	#data = band.ReadAsArray(0, 0, cols, rows)
+	data = band.ReadAsArray(0, 0, cols, rows)
 	for point in points_list:
 	    col = int((point[0] - xOrigin) / pixelWidth)
 	    row = int((yOrigin - point[1] ) / pixelHeight)
-    return row,col #, data[row][col]
+    return row,col, data[row][col]
 
 
 transformations = {}
-for img_id in image_lat_longs_w_id[ids]:
+for img_id in onlyfiles:
+	print(img_id)
 	xOrigin, yOrigin, pixelWidth, pixelHeight, cols, rows = getTransform(img_id)
 	transformations[img_id] = [xOrigin, yOrigin, pixelWidth, pixelHeight, cols, rows]
 
-for entry in study_area['geometry']:
+for entry in study_area['geometry'][0:10]:
 	min_max_coords = [entry.bounds]
 	img_id = entry['img_id']
 	entry['geometry']
