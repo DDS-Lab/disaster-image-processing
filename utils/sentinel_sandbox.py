18bbc35b14ce67ca4567b47bf23c166949c9e374
argideritzalpea <crh2ke@virginia.edu>
2018-07-09 11:30:42 -0700

    almost works

--- utils/sentinel_sandbox.py
+++ utils/sentinel_sandbox.py
@@ -12,7 +12,7 @@ from os.path import isfile, join
 
 #from mpl_toolkits.basemap import Basemap  # Avaliable here: https://github.com/matplotlib/basemap
 
-inputfile = '/Users/Chris/Desktop/hey/dssg/test2.json'
+inputfile = '/Users/Chris/Desktop/hey/dssg/coordinateandtif.json'
 tiffolder = '/Users/Chris/Desktop/hey/dssg/sample_tiff/'
 image_lat_longs_w_id = '/Users/Chris/Desktop/hey/dssg/tifRange-tiles-run-1.csv'
 onlyfiles = [f for f in listdir(tiffolder) if isfile(join(tiffolder, f)) and f.endswith('.tif')]
@@ -23,14 +23,17 @@ with open(image_lat_longs_w_id, 'r') as f:
 	image_ids = [r for r in reader]
 
 testd = image_ids[0]
+img_ids_only = set()
+for i in image_ids:
+	img_ids_only.add(i[0])
 
 #smalltiffidfile = '/Users/Chris/Desktop/hey/dssg/tifRange-tiles-run-1.csv'
 # image_lat_longs_w_id = csv.DictReader(open("image_id_ll.csv"))
 driver = gdal.GetDriverByName('GTiff')
 
-## get id of imag
+## Read json file and get all entries
 geo_json = gpd.read_file(inputfile)
-study_area = geo_json.copy()
+bounding_boxes = geo_json.copy()
 
 def getTransform(img_id):
 	path = tiffolder + '/' + img_id
@@ -44,14 +47,14 @@ def getTransform(img_id):
 	yOrigin = transform[3]
 	pixelWidth = transform[1]
 	pixelHeight = -transform[5]
-	return xOrigin, yOrigin, pixelWidth, pixelHeight, cols, rows
+	return xOrigin, yOrigin, pixelWidth, pixelHeight, cols, rows, band
 
-def convert(xOrigin, yOrigin, pixelWidth, pixelHeight, cols, rows):
+def convert(xOrigin, yOrigin, pixelWidth, pixelHeight, cols, rows, band):
 	data = band.ReadAsArray(0, 0, cols, rows)
 	for point in points_list:
 	    col = int((point[0] - xOrigin) / pixelWidth)
 	    row = int((yOrigin - point[1] ) / pixelHeight)
-    return row,col, data[row][col]
+	return row,col, data[row][col]
 
 
 transformations = {}
@@ -60,18 +63,21 @@ for img_id in onlyfiles:
 	xOrigin, yOrigin, pixelWidth, pixelHeight, cols, rows = getTransform(img_id)
 	transformations[img_id] = [xOrigin, yOrigin, pixelWidth, pixelHeight, cols, rows]
 
-for entry in study_area['geometry'][0:10]:
-	min_max_coords = [entry.bounds]
-	img_id = entry['img_id']
-	entry['geometry']
-	transform = transformations[img_id]
+for _, entry in bounding_boxes.ix[0:10].iterrows():
+	min_max_coords = [entry['geometry'].bounds]
+	tif_id = entry['tif_id']
+	if tif_id not in img_ids_only or tif_id not in onlyfiles:
+		print('hey')
+		pass
+	transform = transformations[tif_id]
 	xOrigin = transform[0]
 	yOrigin = transform[1]
 	pixelWidth = transform[2]
 	pixelHeight = transform[3]
 	cols = transform[4]
 	rows = transform[5]
-	convert(xOrigin, yOrigin, pixelWidth, pixelHeight, cols, rows)
+	band = transform[6]
+	convert(xOrigin, yOrigin, pixelWidth, pixelHeight, cols, rows, band)
 
 
 
