f21e972140a61732f9baee31a5ad32ae86c77d7a
aescay <andrew.escay@gmail.com>
2018-08-02 11:31:29 -0700

    Add argparse to flatten_geojson.py

--- pixel-conversion/flatten_geojson.py
+++ pixel-conversion/flatten_geojson.py
@@ -2,7 +2,7 @@
 This script get the output of Sean's QGIS process and flattens the output in
 order to be runnable on other scripts
 '''
-
+import argparse
 import geopandas as gpd
 
 
@@ -12,7 +12,12 @@ def keep_rows_with_values(df, row_name):
     return df_temp
 
 
-df = gpd.read_file('data_training/boundingboxes/boundingboxes_damaged.geojson')
+parser = argparse.ArgumentParser()
+parser.add_argument('input', help="name the geojson you want flat")
+parser.add_argument('output', help="name what you want it called")
+args = parser.parse_args()
+
+df = gpd.read_file('../data_training/boundingboxes/' + args.input)
 
 dates = ['files_0827', 'files_0828', 'files_0829', 'files_0830', 'files_0831',
          'files_0901', 'files_0902', 'files_0903']
@@ -20,7 +25,7 @@ flat_df = gpd.GeoDataFrame(columns=['id', 'damagelevel', 'geometry', 'image'])
 for date in dates:
     temp_df = keep_rows_with_values(df, date)
     flat_df = flat_df.append(temp_df, ignore_index=True)
-flat_df
+flat_df.id = flat_df.index
 
-flat_df.to_file('data_training/boundingboxes/boundingboxes_flat.geojson',
+flat_df.to_file('../data_training/boundingboxes/' + args.output,
                 driver='GeoJSON')
